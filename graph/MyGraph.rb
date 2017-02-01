14f1c4cafe7793961e95ffdedfaed2df31dc50ce
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-02-01 10:01:50 -0500

    Add hashes

--- graph/MyGraph.rb
+++ graph/MyGraph.rb
@@ -5,20 +5,21 @@ require_relative 'MyVertex'
 class MyGraph
   attr_accessor :label
 
-  def initialize(label = nil)
+  def initialize(options)
     @label = label
     @graph = Hash.new
-    @verteces = Array.new
+    @options = Hash.new
   end
 
-  def new_vertex(options = {})
+  def new_vertex(options)
     vertex = MyVertex.new(options)
-    @verteces.push(vertex)
+    @graph[vertex] = {}
   end
 
-  def new_edge(w, v, options = {})
+  def new_edge(w, v, options)
     edge = MyEdge.new(options)
-    # { vertex => [[vertex, 150], [vertex, 75], [vertex, 90]] }
+    graph[v] = {w => edge}
+    graph[w] = {v => edge}
   end
 
   def remove_edge(v)
@@ -30,7 +31,7 @@ class MyGraph
   end
 
   def list_vertices()
-    @verteces
+    @verteces.keys
   end
 end
 
@@ -41,3 +42,4 @@ my_graph.new_vertex(label: "c")
 my_graph.new_vertex(value: 4)
 my_graph.new_vertex(value: 5)
 puts my_graph.num_vertices
+puts my_graph.list_vertices
