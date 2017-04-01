552079af322a8a9241e1e8a09fffc6833f34658f
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-03-31 22:19:40 -0400

    Add benchmarking

--- fractional/Fractional_test.rb
+++ fractional/Fractional_test.rb
@@ -25,3 +25,21 @@ five.weight = 1
 
 set = [one, two, three, four, five]
 f.fractional_knapsack(set, 100)
+
+file = File.open('output.csv', 'w')
+
+maxheap = MaxHeap.new
+(1..10).each do |size|
+  maxheap.clear
+  (size*10000).times do
+    maxheap.push(rand(10000))
+  end
+  time = Benchmark.measure do |i|
+    10.times do
+      maxheap.max!
+    end
+  end
+  file.write("#{size*10000},#{time.real/10}\n")
+end
+
+file.close
