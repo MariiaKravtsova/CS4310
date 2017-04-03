85cf6a2bc3f0aaf18ede21a0ab680ec8a2313965
Mariia Kravtsova <MariiaKravtsova@users.noreply.github.com>
2017-04-03 17:35:46 -0400

    Update analysis.py

--- fractional/analysis.py
+++ fractional/analysis.py
@@ -11,11 +11,11 @@ with open('outputsort-30.csv','r') as csvfile:
         x.append(int(row[0]))
         y.append(float(row[1]))
 
-coefficients = np.polyfit(np.x*log(x),y,1) # Use log(x) as the input to polyfit.
+coefficients = np.polyfit(np.log(x),y,1) # Use log(x) as the input to polyfit.
 fit = np.poly1d(coefficients) 
 
 plt.plot(x,y, 'ro', label='10-300k')
-plt.plot(x,fit(np.x*log(x)),"--", label="fit")
+plt.plot(x,fit(np.log(x)),"--", label="fit")
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Interesting Graph\nCheck it out')
